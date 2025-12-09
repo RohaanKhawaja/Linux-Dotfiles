cb135d2775e14eb425c318bbb7db3ba3ef1329de
Rohaan Khawaja <rohaankhawaja@outlook.com>
2025-12-09 16:37:08 +0000

    Removing addtional files not needed for WSl

--- shell/scripts/layouts/docked.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Docked: Laptop + 2 external monitors
-hyprctl keyword monitor "eDP-1,2560x1440@240,0x0,1.6"       #Laptop Screen
-hyprctl keyword monitor "DP-3,1920x1080@144,1600x0,1.0"     #Main Monitor 
-hyprctl keyword monitor "DP-4,1920x1080@144,1600x1080,1.0"  #Portable Monitor
-hyprctl keyword monitor "HDMI-A-1,disable"
--- shell/scripts/layouts/laptop.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Laptop only
-hyprctl keyword monitor "eDP-1,2560x1440@240,0x0,1.6"
-hyprctl keyword monitor "DP-3,disable"
-hyprctl keyword monitor "DP-4,disable"
-hyprctl keyword monitor "HDMI-A-1,disable"
-
